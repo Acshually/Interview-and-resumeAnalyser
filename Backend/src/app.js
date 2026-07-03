5f1ab5566d605078860f5fb8c4786c30223e397f
Akshat Chauhan <akshataaryan511@gmail.com>
2026-07-04 00:04:43 +0530

    updated CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,7 +3,17 @@ const cookieParser = require("cookie-parser")
 const cors = require('cors')
 const app = express()
 
-app.use(cors({origin:'http://localhost:5173',credentials:true}))
+const allowedOrigins = ['http://localhost:5173', process.env.FRONTEND_URL];
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin) || origin.includes('render.com')) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+}))
 app.use(express.json())
 app.use(cookieParser())
 
